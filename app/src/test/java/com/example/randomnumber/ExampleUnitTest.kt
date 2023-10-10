3eb732ac37a7ea941277de29ee4d342d88f27b4d
Screamer <vanagoncharov@mail.ru>
2023-10-10 11:32:11 +0300

    +1 Unit Test

--- app/src/test/java/com/example/randomnumber/ExampleUnitTest.kt
+++ app/src/test/java/com/example/randomnumber/ExampleUnitTest.kt
@@ -6,9 +6,24 @@ import org.junit.Assert.*
 
 class ExampleUnitTest {
     @Test
-    fun checkRequire_test(){
-        val generator = Generator("1","10")
+    fun checkRequire_test() {
+        val generator = Generator("1", "10")
         val actual = generator.checkRequire()
+        val expected = true
+
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun generate_test() {
+        val editText1 = 1
+        val editText2 = 10
+        val userInput1 = editText1.toString()
+        val userInput2 = editText2.toString()
+        val generator = Generator(userInput1, userInput2)
+        val result = generator.generate()
+
+        //val actual = editText1 < result
+        val actual = result < editText2
         val expected = true
 
         assertEquals(expected, actual)
